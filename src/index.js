42e567b52b7c0fd920fbb76081c6645b5c58347c
ulrichquatess <ulrichquatess24@gmail.com>
2018-07-09 09:00:12 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+//this part below is where we write our code
+// to run on the browser
+
+ReactDom.render(
+	<h1>Welcome To Quatess Studio</h1>,
+	document.getElementById('root')
+	);
